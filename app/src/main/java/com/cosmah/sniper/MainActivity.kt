c03cc23dcf1c0a5891cb67a242d36dfadc791f0a
cosmah <cosmahke4@gmail.com>
2024-07-09 19:48:39 +0300

    first commit

--- app/src/main/java/com/cosmah/sniper/MainActivity.kt
+++ app/src/main/java/com/cosmah/sniper/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SniperTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Cosmah",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
